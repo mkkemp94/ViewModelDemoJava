897b3f82029734e13b8362ed4f22dcb265d9aebc
MKemp <mkemp@accu-time.com>
2020-09-23 10:18:38 -0400

    Add data binding

--- app/src/main/java/com/mkemp/viewmodeldemojava/MainActivity.java
+++ app/src/main/java/com/mkemp/viewmodeldemojava/MainActivity.java
@@ -2,32 +2,31 @@ package com.mkemp.viewmodeldemojava;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
+import com.mkemp.viewmodeldemojava.databinding.ActivityMainBinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 public class MainActivity extends AppCompatActivity
 {
     private int count = 0;
+    private ActivityMainBinding binding;
     
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    
-        final TextView textView = findViewById(R.id.count_text);
-        Button button = findViewById(R.id.button);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         
-        textView.setText(String.valueOf(count));
+        binding.countText.setText(String.valueOf(count));
         
-        button.setOnClickListener(new View.OnClickListener() {
+        binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
                 count++;
-                textView.setText(String.valueOf(count));
+                binding.countText.setText(String.valueOf(count));
             }
         });
     }

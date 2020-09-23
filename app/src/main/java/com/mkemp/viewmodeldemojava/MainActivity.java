9973901cef239a7d36c5d5d3cbf9f1d9369cf811
MKemp <mkemp@accu-time.com>
2020-09-23 11:03:08 -0400

    Live data with view binding

--- app/src/main/java/com/mkemp/viewmodeldemojava/MainActivity.java
+++ app/src/main/java/com/mkemp/viewmodeldemojava/MainActivity.java
@@ -1,42 +1,27 @@
 package com.mkemp.viewmodeldemojava;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.mkemp.viewmodeldemojava.databinding.ActivityMainBinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity
 {
-    private ActivityMainBinding binding;
-    private MainActivityViewModel viewModel;
+    ActivityMainBinding binding;
+    MainActivityViewModel viewModel;
     
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        
+    
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         
-        viewModel.getCountLiveData().observe(this, new Observer<Integer>() {
-            @Override
-            public void onChanged(Integer count)
-            {
-                binding.countText.setText(String.valueOf(count));
-            }
-        });
-        
-        binding.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                viewModel.incrementCount();
-            }
-        });
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
+        binding.setViewModel(viewModel);
     }
 }

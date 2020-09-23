604796fd4ef19b7beb81a818feac23094285af46
MKemp <mkemp@accu-time.com>
2020-09-23 10:16:22 -0400

    Brute force implementation of counter

--- app/src/main/java/com/mkemp/viewmodeldemojava/MainActivity.java
+++ app/src/main/java/com/mkemp/viewmodeldemojava/MainActivity.java
@@ -1,16 +1,34 @@
 package com.mkemp.viewmodeldemojava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+    private int count = 0;
     
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    
+        final TextView textView = findViewById(R.id.count_text);
+        Button button = findViewById(R.id.button);
+        
+        textView.setText(String.valueOf(count));
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                count++;
+                textView.setText(String.valueOf(count));
+            }
+        });
     }
 }

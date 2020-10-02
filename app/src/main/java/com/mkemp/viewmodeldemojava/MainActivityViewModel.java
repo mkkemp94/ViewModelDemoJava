9c6f3bbe765cbed1913fa0d686bd6097c5881773
MKemp <mkemp@accu-time.com>
2020-10-02 13:47:09 -0400

    Add two way data binding with edit text to add to counter

--- app/src/main/java/com/mkemp/viewmodeldemojava/MainActivityViewModel.java
+++ app/src/main/java/com/mkemp/viewmodeldemojava/MainActivityViewModel.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel;
 public class MainActivityViewModel extends ViewModel
 {
     private MutableLiveData<Integer> mutableCount = new MutableLiveData<>();
+    public MutableLiveData<String> numberToAdd = new MutableLiveData<>();
     
     public MainActivityViewModel(int count)
     {
@@ -18,12 +19,43 @@ public class MainActivityViewModel extends ViewModel
         return mutableCount;
     }
     
-    public void incrementCount()
+    public void setCount()
+    {
+        int newCount = calculateNewCount();
+        mutableCount.setValue(newCount);
+    }
+    
+    private int calculateNewCount()
     {
         Integer oldValue = mutableCount.getValue();
-        if ( oldValue != null )
+        if ( oldValue == null )
         {
-            mutableCount.setValue(oldValue + 1);
+            return 0;
+        }
+        else
+        {
+            Integer numberToAdd = getNumberToAdd();
+            if (numberToAdd != null)
+            {
+                return oldValue + numberToAdd;
+            }
+            else
+            {
+                return ++oldValue;
+            }
+        }
+    }
+    
+    private Integer getNumberToAdd()
+    {
+        String value = numberToAdd.getValue();
+        if ( value == null || value.isEmpty() )
+        {
+            return null;
+        }
+        else
+        {
+            return Integer.parseInt(value);
         }
     }
 }
